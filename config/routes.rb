df01d4b438066f54ff5f3886c072a1570a75194d
karen-2024 <177392295+karen-2024@users.noreply.github.com when>
2024-11-28 14:40:29 +0000

    [Add]各ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root to: "homes#top"
-  
+
+  resources :posts
+  resources :users, only: [:show, :edit, :update]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_28_122347) do
+ActiveRecord::Schema.define(version: 2024_11_28_124328) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,22 @@ ActiveRecord::Schema.define(version: 2024_11_28_122347) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.integer "amount"
+    t.string "package"
+    t.integer "price"
+    t.integer "prefecture"
+    t.string "location"
+    t.integer "recommend"
+    t.string "title"
+    t.text "review"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
