092c77dc3386f7fb75956b32bdae3e4583a2970e
karen-2024 <177392295+karen-2024@users.noreply.github.com>
2025-01-01 13:17:17 +0000

    [Install]dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,12 @@ puts "seedの実行を開始"
 
 olivia = User.find_or_create_by!(email: "olivia@gameil.com") do |user|
   user.name = "Olivia"
-  user.password = "password"
+  user.password = "olivia24"
+end
+
+admin = User.find_or_create_by!(email: "admin@example.com") do |user|
+  user.name = "管理者"
+  user.password = "#{ENV['SECRET_KEY']}"
 end
 
 puts "seedの実行が完了しました"
